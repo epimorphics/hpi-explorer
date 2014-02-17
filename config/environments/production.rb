7191cd5feeea98b81a0e2a23428f19c55949066a
Ian Dickinson <i.j.dickinson@gmail.com>
2014-02-17 15:43:50 +0000

    Added asset precompile instruction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,13 @@ HpiExplorer::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(
+    application.css
+    application-ie8.css
+    application-ie7.css
+    application-ie6.css
+    application.js
+  )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
