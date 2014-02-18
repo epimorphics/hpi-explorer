13790deadbd2c4721286d59699755afc7229a563
Ian Dickinson <i.j.dickinson@gmail.com>
2014-02-18 22:04:33 +0000

    Added basic acceptance tests for HTML-only form. Form updated to incorporate feedback from wireframe review 18th Feb.

--- test/integration/index_page_test.rb
+++ test/integration/index_page_test.rb
@@ -1,9 +1,31 @@
 
 class IndexAcceptanceTest < AcceptanceSpec
 
-  it "should have a place to search by county" do
+  it "should have input controls to search by place" do
     visit "/"
-    find( "input[name='region1']" ).wont_be_nil
+    page.has_css?( "select[name='country1']" ).must_be_same_as true
+
+    %w(region1 county_or_ua1 borough1).each do |place|
+      page.has_css?( "input[type='text'][name='#{place}']" ).must_be_same_as true
+    end
   end
 
+  it "will not (yet) have a place to make a comparison search" do
+    visit "/"
+    page.has_css?("select[name='country2']").must_be_same_as false
+  end
+
+  it "will have controls to select indicators" do
+    visit "/"
+    %w(m_hpi m_ap m_chm m_chm m_vol).each do |measure|
+      page.has_css?( "input[type='checkbox'][name='#{measure}']" ).must_be_same_as true
+    end
+  end
+
+  it "will only have the hpi selected by default" do
+    visit "/"
+    %w(m_hpi m_ap m_chm m_chm m_vol).each do |measure|
+      page.has_checked_field?(measure).must_be_same_as (measure == "m_hpi")
+    end
+  end
 end
