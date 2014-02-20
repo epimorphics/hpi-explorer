2b9f2d8bd55b22ad9f29eb00f493ef85ec38b5e7
Ian Dickinson <i.j.dickinson@gmail.com>
2014-02-20 18:29:10 +0000

    Tests on UserPreferences

--- app/models/user_preferences.rb
+++ app/models/user_preferences.rb
@@ -1,26 +1,54 @@
 # A model object to encapsulate the search preferences we have elicited from the user
 class UserPreferences
+  INDEX_DEFINITIONS = {
+    m_hpi:  {aspect: "hpi:indicesSASM", label: "Index"},
+    m_ap:   {aspect: "hpi:averagePricesSASM", label: "Average price"},
+    m_avd:  {aspect: "hpi:averagePricesSASM", label: "Average price (detached)"},
+    m_avsd: {aspect: "hpi:averagePricesSASM", label: "Average price (semi-detached"},
+    m_avt:  {aspect: "hpi:averagePricesSASM", label: "Average price (terraced)"},
+    m_avf:  {aspect: "hpi:averagePricesSASM", label: "Average price (flats)"},
+    m_chm:  {aspect: "hpi:monthlyChange", label: "Monthly change"},
+    m_chy:  {aspect: "hpi:yearlyChange", label: "Yearly change"},
+    m_vol:  {aspect: "hpi:salesVolume", label: "Sales volume"}
+  }
+
   attr_reader :params
 
-  def initialize( params )
-    @params = params
+  def initialize( params = {} )
+    @params = indifferent_access( params )
   end
 
+  # Return the name of the partial to use to layout a second search area
+  # for comparisons
   def area_comparison_partial
     "hpi/add_second_area_selection"
   end
 
+  # Return the name of the currently selected location
   def selected_location_name
     param( :loc )
   end
 
+  # Return true if the given index is currently selected
   def selected_index?( index_name, val = nil )
-    @params[index_name] && (!val || @params[index_name] == val)
+    !!(@params[index_name] && (!val || @params[index_name] == val))
   end
 
+  # Return an array of the index definitions if that index is
+  # currently selected
+  def selected_indices
+    INDEX_DEFINITIONS.select {|key, index| param(key)} .values
+  end
+
+  # Return the non-empty value of parameter p, or nil
   def param( p )
-    pp = params[p]
-    (pp && pp.length > 0) ? pp : nil
+    ((pp = params[p].to_s) && pp.length > 0) ? pp : nil
+  end
+
+  private
+
+  def indifferent_access( h )
+    h.is_a?( HashWithIndifferentAccess) ? h : HashWithIndifferentAccess.new( h )
   end
 
 end
--- test/models/user_preferences_test.rb
+++ test/models/user_preferences_test.rb
@@ -1,12 +1,28 @@
 require 'test_helper'
 describe "UserPreferences" do
-  it "should set the refCountryName selector and generate a query" do
-    user_prefs = UserPreferences.new( "country1" => "England and Wales" )
 
-    user_prefs.locations.must_equal [{"hpi:refCountryName" => "England and Wales"}]
+  it "should define a partial for the area comparison" do
+    prefs = UserPreferences.new
+    prefs.area_comparison_partial.wont_be_nil
+  end
 
-    user_prefs.query.must_not_be_nil
-    user_prefs.query.to_json.must_match_json_expression {"hpi:refRegion"}
+  it "should report the selected location" do
+    UserPreferences.new.selected_location_name.must_be_nil
+    UserPreferences.new({"loc" => ""}).selected_location_name.must_be_nil
+    UserPreferences.new({"loc" => "foo"}).selected_location_name.must_equal "foo"
+  end
 
+  it "should report when an index is selected" do
+    up = UserPreferences.new( {"m_hpi" => "1", "m_ap" => "true"} )
+    up.selected_index?( :m_hpi ).must_equal true
+    up.selected_index?( "m_hpi" ).must_equal true
+    up.selected_index?( "m_hpi", "1" ).must_equal true
+    up.selected_index?( "m_hpi", "2" ).must_equal false
+    up.selected_index?( "m_vol" ).must_equal false
+  end
+
+  it "should report the selected indices" do
+    up = UserPreferences.new( {"m_hpi" => "1"} )
+    up.selected_indices.must_equal [{aspect: "hpi:indicesSASM", label: "Index"}]
   end
 end
