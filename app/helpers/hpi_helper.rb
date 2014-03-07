041a066f533a8d43f497782fb061f7570c3efa67
Ian Dickinson <i.j.dickinson@gmail.com>
2014-03-07 02:21:53 +0000

    User story 29 - navigate via the region hierarchy

--- app/helpers/hpi_helper.rb
+++ app/helpers/hpi_helper.rb
@@ -1,2 +1,42 @@
 module HpiHelper
+  def list_related_locations( preferences )
+    output = []
+
+    (0..1).each do |i|
+      output << _list_related_locations( preferences.search_id( i ), preferences )
+    end
+
+    output.join( "<br />" ).html_safe
+  end
+
+  def _list_related_locations( search_id, preferences )
+    if uri = preferences.selected_location_uri( search_id )
+      lc = Location.new( uri )
+
+      within = render_place_link( lc.within, preferences, search_id ) if lc.within
+      contains = lc.contains.map {|place| render_place_link( place, preferences, search_id )}
+      name = preferences.selected_location_name( search_id )
+
+      if within && contains.length > 0
+        str = "#{name} is part of #{within}, and contains: #{contains.join( ", " )}"
+      elsif within
+        str = "#{name} is part of #{within}"
+      elsif contains.length > 0
+        str = "#{name} contains: #{contains.join( ", " )}"
+      else
+        str = nil
+      end
+
+      str.html_safe if str
+    end
+  end
+
+  def render_place_link( place, preferences, search_id )
+    loc = preferences.attribute_with_search_id( "loc", search_id )
+    loc_uri = preferences.attribute_with_search_id( "loc_uri", search_id )
+    name = (place["label"]).kind_of?(Hash) ? place["label"]["@value"] : place["label"]
+
+    link_to( name, preferences.as_path( :hpi, {loc => name, loc_uri => place["@id"]} ))
+  end
+
 end
---
+++ app/models/location.rb
@@ -0,0 +1,51 @@
+# The locations command object provides an API for traversing the
+# location hierarchy in the HPI dataset
+class Location < DataService
+  attr_reader :json
+
+  def initialize( location_uri )
+    super
+    @id = location_uri
+    puts "@id #{@id}"
+    @hpi = dataset( :hpi )
+    puts "@hpi #{@hpi}"
+    @json = @hpi.describe( location_uri )
+    puts "@json #{@json}"
+    @labels = {location_uri => @json["label"]}
+    puts "@labels #{@labels}"
+  end
+
+  def within
+    labelled( json["within"] )
+  end
+
+  def contains
+    Array( json["contains"] ).map {|v| labelled( v )}
+  end
+
+  def name
+    label_for( @id )
+  end
+
+  def labelled( uri )
+    return nil unless uri
+    uri.kind_of?( Hash ) ? uri : {"@id" => uri, "label" => label_for( uri )}
+  end
+
+  def label_for( uri, lang = "en" )
+    # binding.pry if uri =~ /torbay/i
+    l = labels( uri ).find {|l| l.kind_of?(Hash) ? l["@language"] == lang : true}
+    return l unless l.kind_of?( Hash )
+    l["@value"]
+  end
+
+  def labels( uri )
+    unless @labels.has_key?( uri )
+      l = @hpi.describe( uri )["label"]
+      @labels[uri] = l.kind_of?( Array ) ? l : [l]
+    end
+    @labels[uri]
+  end
+
+
+end
