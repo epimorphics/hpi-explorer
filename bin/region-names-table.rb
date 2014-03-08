034c8104279a47bcc87a25dfb732c483e85e5a41
Ian Dickinson <i.j.dickinson@gmail.com>
2014-03-08 10:58:06 +0000

    Added lookup by GSS code to locations table

--- bin/region-names-table.rb
+++ bin/region-names-table.rb
@@ -9,29 +9,38 @@ sresults["results"]["bindings"].each do |result|
   l = result["label"]
   p = result["parent"]
   c = result["child"]
+  s = result["same"]
 
   if l["xml:lang"] == "en"
     unless locations[v]
       locations[v] = {children: Set.new}
     end
 
-    locations[v][:label] = l["value"]
-    locations[v][:parent] = p["value"] if p
-    locations[v][:children] << c["value"] if c
+    location = locations[v]
+    location[:label] = l["value"]
+    location[:parent] = p["value"] if p
+    location[:children] << c["value"] if c
+
+    if s && s["value"] =~ /(E\d+)$/
+      location[:gss] = $1
+    end
   end
 end
 
 location_names = []
+gss_index = Hash.new
 
 locations.each do |uri, properties|
   location_names << {value: uri, label: properties[:label]}
   properties[:children] = properties[:children].to_a
+  gss_index[properties[:gss]] = uri if properties[:gss]
 end
 
 location_names.sort! {|s0,s1| s0[:label] <=> s1[:label]}
 
 puts "var HpiLocations = function() {"
-puts "var locationNames = #{location_names.to_json}"
-puts "var locations = #{locations.to_json}"
-puts "return {locationNames: locationNames, locations: locations};"
+puts "var locationNames = #{location_names.to_json};"
+puts "var locations = #{locations.to_json};"
+puts "var gssIndex = #{gss_index.to_json};"
+puts "return {locationNames: locationNames, locations: locations, gssIndex: gssIndex};"
 puts "}();"
